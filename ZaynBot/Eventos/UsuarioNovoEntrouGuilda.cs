df148a6dda1467ac5df5cb3dd8a9f03c96e0b715
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2019-05-01 00:11:34 -0300

    Removido mensagem de boas vindas

--- ZaynBot/Eventos/UsuarioNovoEntrouGuilda.cs
+++ ZaynBot/Eventos/UsuarioNovoEntrouGuilda.cs
@@ -10,16 +10,16 @@ namespace ZaynBot.Eventos
     {
         public static async Task EventoBemVindoAsync(GuildMemberAddEventArgs e)
         {
-            if (e.Guild.Id == 508615273515843584)
-            {
-                DiscordChannel f = e.Guild.GetChannel(551469878268395530);
-                await f.SendMessageAsync(string.Format(new MensagensEventoBemVindo().Sortear(), e.Member.Mention) + $"\nNão se esqueça de ler as regras!");
-            }
-            if (e.Guild.Id == 420044060720627712)
-            {
-                DiscordChannel f = e.Guild.GetChannel(423347465912320000);
-                await f.SendMessageAsync($"Bem-vindo {e.Member.Mention} ao Dragon and Zayn's RPG!");
-            }
+            //if (e.Guild.Id == 508615273515843584)
+            //{
+            //    DiscordChannel f = e.Guild.GetChannel(551469878268395530);
+            //    await f.SendMessageAsync(string.Format(new MensagensEventoBemVindo().Sortear(), e.Member.Mention) + $"\nNão se esqueça de ler as regras!");
+            //}
+            //if (e.Guild.Id == 420044060720627712)
+            //{
+            //    DiscordChannel f = e.Guild.GetChannel(423347465912320000);
+            //    await f.SendMessageAsync($"Bem-vindo {e.Member.Mention} ao Dragon and Zayn's RPG!");
+            //}
         }
     }
 
@@ -28,13 +28,11 @@ namespace ZaynBot.Eventos
         List<string> Mensagens;
         public MensagensEventoBemVindo()
         {
-            Mensagens = new List<string>
-            {
-                "Batatinha quando nasce espalha rama pelo chão, {0} chegou na área para trazer animação!",
-                "{0} acabou de entrar. Finjam que estão ocupados!",
-                "Estávamos esperando por você, {0}.",
-                "{0} está aqui, conforme a profecia."
-            };
+            Mensagens = new List<string>();
+            Mensagens.Add("Batatinha quando nasce espalha rama pelo chão, {0} chegou na área para trazer animação!");
+            Mensagens.Add("{0} acabou de entrar. Finjam que estão ocupados!");
+            Mensagens.Add("Estávamos esperando por você, {0}.");
+            Mensagens.Add("{0} está aqui, conforme a profecia.");
         }
 
         public string Sortear()
