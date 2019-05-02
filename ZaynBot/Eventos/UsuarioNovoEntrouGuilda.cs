5531745274383c6d9c94262219757d23e6f92080
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2019-05-01 22:23:44 -0300

    Refatoração de mensagem de boas vindas

--- ZaynBot/Eventos/UsuarioNovoEntrouGuilda.cs
+++ ZaynBot/Eventos/UsuarioNovoEntrouGuilda.cs
@@ -18,27 +18,24 @@ namespace ZaynBot.Eventos
             if (e.Guild.Id == 420044060720627712)
             {
                 DiscordChannel f = e.Guild.GetChannel(423347465912320000);
-                await f.SendMessageAsync(string.Format(new MensagensEventoBemVindo().Sortear(), e.Member.Mention));
+                await f.SendMessageAsync(string.Format(MensagensEventoBemVindo.Sortear(), e.Member.Mention));
 
             }
             await Task.CompletedTask;
         }
     }
 
-    public class MensagensEventoBemVindo
+    public static class MensagensEventoBemVindo
     {
-        List<string> Mensagens;
-        public MensagensEventoBemVindo()
+        public static string Sortear()
         {
-            Mensagens = new List<string>();
-            Mensagens.Add("Batatinha quando nasce espalha rama pelo chão, {0} chegou na área para trazer animação!");
-            Mensagens.Add("{0} acabou de entrar. Finjam que estão ocupados!");
-            Mensagens.Add("Estávamos esperando por você, {0}.");
-            Mensagens.Add("{0} está aqui, conforme a profecia.");
-        }
-
-        public string Sortear()
-        {
+            List<string> Mensagens = new List<string>
+            {
+                "Batatinha quando nasce espalha rama pelo chão, {0} chegou na área para trazer animação!",
+                "{0} acabou de entrar. Finjam que estão ocupados!",
+                "Estávamos esperando por você, {0}.",
+                "{0} está aqui, conforme a profecia."
+            };
             Random r = new Random();
             return Mensagens[r.Next(0, Mensagens.Count)];
         }
