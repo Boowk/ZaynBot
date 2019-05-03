1fee82bbdad150bec205bbfa95dd9c08b56d82c6
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2019-05-02 23:16:15 -0300

    Nova versão do comando ranque - comando movido para info ranque nivel

--- ZaynBot/Comandos/Informações/ComandosInfo.cs
+++ ZaynBot/Comandos/Informações/ComandosInfo.cs
@@ -9,6 +9,7 @@ namespace ZaynBot.Comandos.Informações
 {
     [Group("info")]
     [Description("Comandos informativos.")]
+    [Cooldown(1, 10, CooldownBucketType.User)]
     public class ComandosInfo
     {
         [Command("ping")]
@@ -17,6 +18,20 @@ namespace ZaynBot.Comandos.Informações
         public async Task ComandoPing(CommandContext ctx)
         {
             await Ping.PingAb(ctx);
+        }
+
+        [Group("ranque")]
+        [Description("Comandos comparativos.")]
+        [Cooldown(1, 10, CooldownBucketType.User)]
+        public class GrupoRanque
+        {
+            [Command("nivel")]
+            [Description("Exibe os maiores niveis do server ou global.")]
+            [Cooldown(1, 10, CooldownBucketType.User)]
+            public async Task ComandoRanqueNivel(CommandContext ctx, [Description("Servidor ou Global")] string server = "servidor")
+            {
+                await TopRanque.TopRanqueAb(ctx, server);
+            }
         }
     }
 }
