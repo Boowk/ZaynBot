6b0670bd9cd8c739a2365a958e9798c35f89a2bf
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2019-05-02 22:38:09 -0300

    Novo comando ajuda

--- ZaynBot/Comandos/Ajuda.cs
+++ ZaynBot/Comandos/Ajuda.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ZaynBot.Comandos
 {
     public class Ajuda
     {
+        [Command("ajuda")]
+        [Aliases("h", "?")]
+        [Description("Exibe os comandos, a descrição do comando, suas abreviações e argumentos.")]
+        [Cooldown(1, 15, CooldownBucketType.User)]
+        public async Task HelpAsync(CommandContext ctx, [Description("Nome do comando")]params string[] comando)
+        {
+            await ctx.CommandsNext.DefaultHelpAsync(ctx, comando);
+        }
     }
 }
