c77f252b1e0d7ee38acde8e32c1310b0633b1903
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2019-05-06 23:47:39 -0300

    Adicionado item

---
+++ ZaynBot/Entidades/EntidadesRpg/Item.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZaynBot.Entidades.EntidadesRpg
+{
+    public class Item
+    {
+        public enum Tipo
+        {
+            Arma,
+            Armadura,
+        }
+
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public Tipo TipoItem { get; set; }
+        public int Quantidade { get; set; }
+        public float PreçoBase { get; set; }
+
+        public Item(string nome, string descrição, Tipo tipo_item, float preco_base, int quantidade = 1)
+        {
+            Nome = nome;
+            Descricao = descrição;
+            TipoItem = tipo_item;
+            PreçoBase = preco_base;
+            Quantidade = quantidade;
+        }
+    }
+}
