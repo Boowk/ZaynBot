4369dca05a786e5b81e584d1de64c89b29513b38
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2019-05-11 20:38:04 -0300

    Adicionado classe sortear

---
+++ ZaynBot/Funções/Sortear.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ZaynBot.Funções
+{
+    public class Sortear
+    {
+        private static Random random;
+
+        public static int Valor(int min, int max)
+        {
+            random = new Random();
+            return random.Next(min, max + 1);
+        }
+
+        public static long Valor(long min, long max)
+        {
+            random = new Random();
+            return (long)random.NextDouble() * (max - min) + min;
+        }
+
+        public static double Valor(double min, double max)
+        {
+            random = new Random();
+            return random.NextDouble() * (max - min) + min;
+        }
+    }
+}
