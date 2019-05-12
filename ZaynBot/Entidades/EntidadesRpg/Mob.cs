a353f518c2bfba12a1651dddb484678e385aa995
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2019-05-11 22:54:58 -0300

    Refatoração: Mobs

--- ZaynBot/Entidades/EntidadesRpg/Mob.cs
+++ ZaynBot/Entidades/EntidadesRpg/Mob.cs
@@ -1,6 +1,4 @@
 ﻿using MongoDB.Bson.Serialization.Attributes;
-using System.Collections.Generic;
-using ZaynBot.Funções;
 
 namespace ZaynBot.Entidades.EntidadesRpg
 {
@@ -17,31 +15,14 @@ namespace ZaynBot.Entidades.EntidadesRpg
         public float AtaqueMagico { get; set; }
         public float DefesaMagica { get; set; }
         public int Velocidade { get; set; }
-        public List<Hit> Hits { get; set; } // Aleatoriamente vai sair um dano que ele tiver e com base no estilo para mostrar ao jogador a fraqueza
+        // public List<Hit> Hits { get; set; } // Aleatoriamente vai sair um dano que ele tiver e com base no estilo para mostrar ao jogador a fraqueza
         public int ChanceDeAparecer { get; set; }
         // public List<ItemChanceCair> ChanceCairItem { get; set; } = new List<ItemChanceCair>();
     }
 
-    public class Hit
-    {
-        public int Hits { get; set; }
-        public string Nome { get; set; }
-    }
-
-    public static class ExtensaoMob
-    {
-        public static Mob Raca(this Mob mob, Raça raca)
-        {
-            Sortear sortear = new Sortear();
-            mob.RaçaMob = raca;
-            mob.PontosDeVida += sortear.Valor(raca.PontosDeVidaBaseMin, raca.PontosDeVidaBaseMax);
-            mob.PontosDeVidaMaxima = mob.PontosDeVida;
-            mob.AtaqueFisico += sortear.Valor(raca.AtaqueFisicoBaseMin, raca.AtaqueFisicoBaseMax);
-            mob.DefesaFisica += sortear.Valor(raca.DefesaFisicaBaseMin, raca.DefesaFisicaBaseMax);
-            mob.AtaqueMagico += sortear.Valor(raca.AtaqueMagicoBaseMin, raca.AtaqueMagicoBaseMax);
-            mob.DefesaMagica += sortear.Valor(raca.DefesaMagicaBaseMin, raca.DefesaMagicaBaseMax);
-            mob.Velocidade += sortear.Valor(raca.VelocidadeBaseMin, raca.VelocidadeBaseMax);
-            return mob;
-        }
-    }
+    //public class Hit
+    //{
+    //    public int Hits { get; set; }
+    //    public string Nome { get; set; }
+    //}
 }
