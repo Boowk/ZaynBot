8fc6d2dfbff3266333d481f36ce497c7e08a5ece
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2019-06-09 15:26:17 -0300

    Removido dependencias nesnecessarias

--- ZaynBot/RPG/Comandos/Viajar/ComandoLeste.cs
+++ ZaynBot/RPG/Comandos/Viajar/ComandoLeste.cs
@@ -1,9 +1,5 @@
 ﻿using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
-using DSharpPlus.Entities;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using ZaynBot.RPG.Entidades;
 using ZaynBot.RPG.Entidades.Enuns;
