37537f7a6aa0c80153eb1e818dd79542871a24cf
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2019-06-18 22:24:24 -0300

    Primeira versão do inventario

---
+++ ZaynBot/RPG/Entidades/RPGInventario.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZaynBot.RPG.Entidades
+{
+    public class RPGInventario
+    {
+        public float PesoMaximo { get; set; }
+        public float PesoAtual { get; set; }
+        public Dictionary<string, RPGItem> Inventario { get; set; } = new Dictionary<string, RPGItem>();
+
+        public RPGInventario(RPGRaça raca)
+        {
+            //PesoMax = raca.atributos * ...
+        }
+
+        public bool Adicionar(RPGItem item)
+        {
+            /* Verifica se existi o item no inventario
+             * se não existir, o adiciona.
+             * se existir, o soma
+            */
+            return true;
+        }
+
+        public bool Destruir(string nome)
+        {
+            return true;
+        }
+
+        public bool Usar(string nome, int quantidade)
+        {
+            return true;
+        }
+    }
+}
