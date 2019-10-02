4ea12fea0c2e29c84813b25514aeb5574c0e2af0
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2019-10-02 15:21:51 -0300

    Movido o comando equipamento para status

--- ZaynBot/RPG/Comandos/Exibir/StatusComando.cs
+++ ZaynBot/RPG/Comandos/Exibir/StatusComando.cs
@@ -2,40 +2,54 @@
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
 using System;
+using System.Text;
 using System.Threading.Tasks;
 using ZaynBot.Core.Atributos;
 using ZaynBot.RPG.Entidades;
+using ZaynBot.RPG.Entidades.Enuns;
 
 namespace ZaynBot.RPG.Comandos.Exibir
 {
     public class StatusComando : BaseCommandModule
     {
         [Command("status")]
-        [Description("Exibe os atributos derivados do seu personagem.")]
-        [UsoAtributo("status")]
+        [Description("Exibe os status e os equipamentos do seu personagem.")]
         [Cooldown(1, 10, CooldownBucketType.User)]
         public async Task StatusComandoAb(CommandContext ctx)
         {
             await ctx.TriggerTypingAsync();
             UsuarioRPG.TryGetPersonagemRPG(ctx, out UsuarioRPG usuario);
             PersonagemRPG personagem = usuario.Personagem;
-            DiscordEmbedBuilder embed = new DiscordEmbedBuilder()
-            {
-                Author = new DiscordEmbedBuilder.EmbedAuthor()
-                {
-                    Name = $"{ctx.User.Username}",
-                    IconUrl = ctx.User.AvatarUrl
-                },
-                Color = DiscordColor.Blue,
-                Timestamp = DateTime.Now,
-                ThumbnailUrl = ctx.User.AvatarUrl,
-            }.AddField("Raça".Titulo(), $"{personagem.Raca.Nome.PrimeiraLetraMaiuscula()}", true)
-            .AddField("Nível".Titulo(), $"Nv.{personagem.NivelAtual}", true)
-            .AddField("Experiencia".Titulo(), $"{personagem.ExpAtual.Texto2Casas()}/{personagem.ExpMax.Texto2Casas()}", true)
-            .AddField("Vida".Titulo(), $"{personagem.VidaAtual.Texto2Casas()}/{personagem.VidaMax.Texto2Casas()}", true)
-            .AddField("Mágia".Titulo(), $"{personagem.MagiaAtual.Texto2Casas()}/{personagem.MagiaMax.Texto2Casas()}", true)
-            .AddField("Fome".Titulo(), $"{personagem.FomeAtual.Texto2Casas()}/{personagem.FomeMax.Texto2Casas()}", true);
+            DiscordEmbedBuilder embed = new DiscordEmbedBuilder().Padrao("Status", ctx);
+            embed.WithColor(DiscordColor.Blue);
+
+            StringBuilder sr = new StringBuilder();
+            sr.AppendLine($"**Vida:** {personagem.VidaAtual.Texto2Casas()}/{personagem.VidaMax.Texto2Casas()}");
+            sr.AppendLine($"**Mágia:** {personagem.MagiaAtual.Texto2Casas()}/{personagem.MagiaMax.Texto2Casas()}");
+            sr.AppendLine($"**Fome:** {personagem.FomeAtual.Texto2Casas()}/{personagem.FomeMax.Texto2Casas()}");
+            GerarEquips(sr, "Arma", TipoItemEnum.Arma, personagem);
+            GerarEquips(sr, "Escudo", TipoItemEnum.Escudo, personagem);
+            GerarEquips(sr, "Helmo", TipoItemEnum.Helmo, personagem);
+            GerarEquips(sr, "Couraça", TipoItemEnum.Couraca, personagem);
+            GerarEquips(sr, "Luvas", TipoItemEnum.Luvas, personagem);
+            GerarEquips(sr, "Botas", TipoItemEnum.Botas, personagem);
+            embed.WithDescription(sr.ToString());
             await ctx.RespondAsync(embed: embed.Build());
+        }
+
+        public void GerarEquips(StringBuilder sr, string nomeExibicao, TipoItemEnum itemEnum, PersonagemRPG personagem)
+        {
+            sr.Append($"**{nomeExibicao}:** ");
+            bool isItem = personagem.Inventario.Equipamentos.TryGetValue(itemEnum, out ItemRPG item);
+            if (isItem)
+                sr.AppendLine($"{item.Nome}({item.Id}) - *Durab. {item.Durabilidade}/{ModuloBanco.ItemGet(item.Id).Durabilidade}*");
+            else
+                sr.AppendLine("Nenhum");
         }
     }
 }
+
+
+//AddField("Raça".Titulo(), $"{personagem.Raca.Nome.PrimeiraLetraMaiuscula()}", true)
+//            .AddField("Nível".Titulo(), $"Nv.{personagem.NivelAtual}", true)
+//            .AddField("Experiencia".Titulo(), $"{personagem.ExpAtual.Texto2Casas()}/{personagem.ExpMax.Texto2Casas()}", true)
