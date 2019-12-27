9d493e8d6e4fa02735348325aed14ce4af5a93dd
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2019-12-27 15:27:36 -0300

    Refatoriar comandoViajar

--- ZaynBot/RPG/Comandos/ComandoViajar.cs
+++ ZaynBot/RPG/Comandos/ComandoViajar.cs
@@ -6,59 +6,7 @@ using ZaynBot.RPG.Entidades;
 
 namespace ZaynBot.RPG.Comandos
 {
-    public class SulComando : BaseCommandModule
-    {
-        [Command("sul")]
-        [Aliases("s")]
-        [Description("Explora a área Sul.")]
-        [Cooldown(1, 6, CooldownBucketType.User)]
-        public async Task Sul(CommandContext ctx)
-        {
-            await new Viajar().ViajarAbAsync(ctx, EnumDirecao.Sul);
-            await Task.CompletedTask;
-        }
-    }
-
-    public class LesteComando : BaseCommandModule
-    {
-        [Command("leste")]
-        [Aliases("l")]
-        [Description("Explora a área leste.")]
-        [Cooldown(1, 6, CooldownBucketType.User)]
-        public async Task Leste(CommandContext ctx)
-        {
-            await new Viajar().ViajarAbAsync(ctx, EnumDirecao.Leste);
-            await Task.CompletedTask;
-        }
-    }
-
-    public class NorteComando : BaseCommandModule
-    {
-        [Command("norte")]
-        [Aliases("n")]
-        [Description("Explora a área Norte.")]
-        [Cooldown(1, 6, CooldownBucketType.User)]
-        public async Task Norte(CommandContext ctx)
-        {
-            await new Viajar().ViajarAbAsync(ctx, EnumDirecao.Norte);
-            await Task.CompletedTask;
-        }
-    }
-
-    public class OesteComando : BaseCommandModule
-    {
-        [Command("oeste")]
-        [Aliases("o")]
-        [Description("Explora a área Oeste.")]
-        [Cooldown(1, 6, CooldownBucketType.User)]
-        public async Task Oeste(CommandContext ctx)
-        {
-            await new Viajar().ViajarAbAsync(ctx, EnumDirecao.Oeste);
-            await Task.CompletedTask;
-        }
-    }
-
-    public class Viajar : BaseCommandModule
+    public class ComandoViajar : BaseCommandModule
     {
         public async Task ViajarAbAsync(CommandContext ctx, EnumDirecao enumDirecao)
         {
@@ -98,6 +46,46 @@ namespace ZaynBot.RPG.Comandos
                 }
             }
             await ctx.RespondAsync($"{ctx.User.Mention} você não pode ir para esta direção!");
+        }
+
+        [Command("oeste")]
+        [Aliases("o")]
+        [Description("Explora a área Oeste.")]
+        [Cooldown(1, 6, CooldownBucketType.User)]
+        public async Task Oeste(CommandContext ctx)
+        {
+            await new ComandoViajar().ViajarAbAsync(ctx, EnumDirecao.Oeste);
+            await Task.CompletedTask;
+        }
+
+        [Command("norte")]
+        [Aliases("n")]
+        [Description("Explora a área Norte.")]
+        [Cooldown(1, 6, CooldownBucketType.User)]
+        public async Task Norte(CommandContext ctx)
+        {
+            await new ComandoViajar().ViajarAbAsync(ctx, EnumDirecao.Norte);
+            await Task.CompletedTask;
+        }
+
+        [Command("leste")]
+        [Aliases("l")]
+        [Description("Explora a área leste.")]
+        [Cooldown(1, 6, CooldownBucketType.User)]
+        public async Task Leste(CommandContext ctx)
+        {
+            await new ComandoViajar().ViajarAbAsync(ctx, EnumDirecao.Leste);
+            await Task.CompletedTask;
+        }
+
+        [Command("sul")]
+        [Aliases("s")]
+        [Description("Explora a área Sul.")]
+        [Cooldown(1, 6, CooldownBucketType.User)]
+        public async Task Sul(CommandContext ctx)
+        {
+            await new ComandoViajar().ViajarAbAsync(ctx, EnumDirecao.Sul);
+            await Task.CompletedTask;
         }
     }
 }
