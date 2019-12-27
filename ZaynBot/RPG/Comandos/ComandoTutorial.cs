f127841f56e903ec647f1ddfd5b2e9b81b273c77
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2019-12-27 16:46:07 -0300

    Melhorar tutorial

--- ZaynBot/RPG/Comandos/ComandoTutorial.cs
+++ ZaynBot/RPG/Comandos/ComandoTutorial.cs
@@ -1,6 +1,7 @@
 ﻿using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
+using System.Text;
 using System.Threading.Tasks;
 using ZaynBot.Core.Atributos;
 
@@ -16,11 +17,14 @@ namespace ZaynBot.RPG.Comandos
         public async Task GroupCommandAsync(CommandContext ctx)
         {
             await ctx.TriggerTypingAsync();
+            StringBuilder str = new StringBuilder();
+            str.AppendLine("Escreva z!tutorial [pagina] para ver um tutorial.".Bold());
+            str.AppendLine("1. Descrição".Bold());
+            str.AppendLine("2. Combate".Bold());
+            str.AppendLine("3. Itens".Bold());
+            str.AppendLine("4. Mensagem automática de boas vindas".Bold());
             DiscordEmbedBuilder embed = new DiscordEmbedBuilder();
-            embed.WithDescription("Escreva z!tutorial [pagina] para ver um tutorial.\n".Bold() +
-                "1. Descrição".Bold() +
-                "2. Combate".Bold() +
-                "3. Itens".Bold());
+            embed.WithDescription(str.ToString());
             await ctx.RespondAsync(embed: embed.Build());
         }
 
@@ -58,6 +62,20 @@ namespace ZaynBot.RPG.Comandos
                 "você vai ficando sem vida, para recuperar sua vida, a unica forma é tomando poções recuperadoras. Você precisa " +
                 "viajar usando `leste`, `oeste`, `norte` ou `sul` até encontrar uma loja de poções. Compre algumas poções com " +
                 "`comprar`, e use-as com `usar-item`. Caso não saiba qual itens é vendido, use `loja`.");
+            await ctx.RespondAsync(embed: embed.Build());
+        }
+
+        [Command("4")]
+        public async Task Pagina4(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+            DiscordEmbedBuilder embed = new DiscordEmbedBuilder();
+            embed.WithDescription("A ZaynRPG também envia mensagens de boas vindas! Para configurar é fácil. " +
+                "Use `mod bem-vindo` [mensagem]. Onde no lugar da mensagem você colocará a sua menasgem desejada! Lembre-se " +
+                "que o bot ira enviar essa mensagem, no local que você usou este comando! Então use este comando em uma area que " +
+                "todos possam ver! Também temos outras funções para ajudar na criação da sua mensagem. Use #Menção para mencionar " +
+                "a pessoa e #Quantidade para saber quantos membros tem no servidor após aquela pessoa ter entrado! Mais dúvidas? Use " +
+                "`ajuda mod bem-vindo`.");
             await ctx.RespondAsync(embed: embed.Build());
         }
     }
