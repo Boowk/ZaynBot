674c7428b3ad958fba09b8a20cfbabac95c9b37e
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2020-01-10 14:21:11 -0300

    Adicionar requisito para usar item

--- ZaynBot/RPG/Comandos/ComandoItemEquipar.cs
+++ ZaynBot/RPG/Comandos/ComandoItemEquipar.cs
@@ -29,31 +29,32 @@ namespace ZaynBot.RPG.Comandos
             if (usuario.Personagem.Mochila.Itens.TryGetValue(nome, out RPGMochilaItemData itemData))
             {
                 RPGItem item = ModuloBanco.GetItem(itemData.Id);
+
                 switch (item.Tipo)
                 {
                     case EnumTipo.ArmaPrimaria:
-                        usuario.Personagem.EquiparItem(item);
-                        await ctx.RespondAsync($"A arma primária [{item.Nome}] foi equipada {ctx.User.Mention}!".Bold());
+                        if (await EquiparItemAsync(ctx, usuario, item))
+                            await ctx.RespondAsync($"A arma primária [{item.Nome}] foi equipada {ctx.User.Mention}!".Bold());
                         break;
                     case EnumTipo.ArmaSecundaria:
-                        usuario.Personagem.EquiparItem(item);
-                        await ctx.RespondAsync($"A arma segundaria [{item.Nome}] foi equipada {ctx.User.Mention}!".Bold());
+                        if (await EquiparItemAsync(ctx, usuario, item))
+                            await ctx.RespondAsync($"A arma segundaria [{item.Nome}] foi equipada {ctx.User.Mention}!".Bold());
                         break;
                     case EnumTipo.Botas:
-                        usuario.Personagem.EquiparItem(item);
-                        await ctx.RespondAsync($"As botas [{item.Nome}] foram equipadas {ctx.User.Mention}!".Bold());
+                        if (await EquiparItemAsync(ctx, usuario, item))
+                            await ctx.RespondAsync($"As botas [{item.Nome}] foram equipadas {ctx.User.Mention}!".Bold());
                         break;
                     case EnumTipo.Peitoral:
-                        usuario.Personagem.EquiparItem(item);
-                        await ctx.RespondAsync($"O peitoral [{item.Nome}] foi equipado {ctx.User.Mention}!".Bold());
+                        if (await EquiparItemAsync(ctx, usuario, item))
+                            await ctx.RespondAsync($"O peitoral [{item.Nome}] foi equipado {ctx.User.Mention}!".Bold());
                         break;
                     case EnumTipo.Helmo:
-                        usuario.Personagem.EquiparItem(item);
-                        await ctx.RespondAsync($"O helmo [{item.Nome}] foi equipado {ctx.User.Mention}!".Bold());
+                        if (await EquiparItemAsync(ctx, usuario, item))
+                            await ctx.RespondAsync($"O helmo [{item.Nome}] foi equipado {ctx.User.Mention}!".Bold());
                         break;
                     case EnumTipo.Luvas:
-                        usuario.Personagem.EquiparItem(item);
-                        await ctx.RespondAsync($"As luvas [{item.Nome}] foram esquipadas {ctx.User.Mention}!".Bold());
+                        if (await EquiparItemAsync(ctx, usuario, item))
+                            await ctx.RespondAsync($"As luvas [{item.Nome}] foram esquipadas {ctx.User.Mention}!".Bold());
                         break;
                     default:
                         await ctx.RespondAsync($"Este [item] não é equipavel {ctx.User.Mention}!".Bold());
@@ -63,6 +64,25 @@ namespace ZaynBot.RPG.Comandos
             }
             else
                 await ctx.RespondAsync($"Você não tem este [item] {ctx.User.Mention}!".Bold());
+        }
+
+        private async Task<bool> EquiparItemAsync(CommandContext ctx, RPGUsuario usuario, RPGItem item)
+        {
+            if (item.ProficienciaNivelRequisito != 1)
+            {
+                usuario.Personagem.Proficiencias.TryGetValue(item.Proficiencia, out RPGProficiencia proff);
+                if (proff.Pontos >= item.ProficienciaNivelRequisito)
+                {
+                    usuario.Personagem.EquiparItem(item);
+                    return true;
+                }
+                else
+                {
+                    await ctx.RespondAsync($"Você precisa de {item.ProficienciaNivelRequisito} pontos em {item.Proficiencia.ToString()} para usar [{item.Nome}] {ctx.User.Mention}!".Bold());
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
