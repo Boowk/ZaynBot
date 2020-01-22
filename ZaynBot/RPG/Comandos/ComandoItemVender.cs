0fe908e48a48219b6b374b7ec57fdb5a9d12261d
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2020-01-21 22:57:12 -0300

    Corrigir texto venda

--- ZaynBot/RPG/Comandos/ComandoItemVender.cs
+++ ZaynBot/RPG/Comandos/ComandoItemVender.cs
@@ -11,7 +11,7 @@ namespace ZaynBot.RPG.Comandos
         [Command("vender")]
         [Aliases("v")]
         [Description("Crie um anúncio no mercado para vender a outros jogadores. Quanto maior o preço, mais demorado é para vender.")]
-        [ComoUsar("vender [+quantidade] [+preço] [nome do item]")]
+        [ComoUsar("vender [+quantidade] [+preço cada unidade] [nome do item]")]
         [Exemplo("vender 2 20 ossos")]
         [Cooldown(1, 15, CooldownBucketType.User)]
         public async Task ComandoVenderAb(CommandContext ctx, int quantidade = -1, int preco = -1, [RemainingText] string itemNome = "")
@@ -63,7 +63,7 @@ namespace ZaynBot.RPG.Comandos
                 new RPGVenda(ctx.User.Id, preco, quantidade, itemNome);
                 jogador.Salvar();
 
-                await ctx.RespondAsync($"{ctx.User.Mention} você anunciou no mercado {quantidade} [{itemNome.FirstUpper()}] por {preco} Zayn! Aguarde algum jogador comprar o seu produto.".Bold());
+                await ctx.RespondAsync($"{ctx.User.Mention} você anunciou no mercado {quantidade} [{itemNome.FirstUpper()}] por {preco} Zayn cada únidade! Aguarde algum jogador comprar o seu produto.".Bold());
             }
             else
                 await ctx.RespondAsync($"{ctx.User.Mention} item [{itemNome.FirstUpper()}] não foi encontrado na mochila!".Bold());
